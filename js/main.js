f4bb15e8058919de84f332ba043c67cd251612da
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-10-05 17:55:12 +0200

    added logo in header, gave it shadow in the css for depth-effect, added social buttons beneath header gave them shadow in css for depth

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Hi Girl Coder, please make sure you join our community at meetup.com/GirlCode. Boys are welcome too!");
+
+$(document).ready(function() {
+  $('.social-button').hover(function() {
+    $(this).effect( 'bounce', { times: 1 }, 'slow');
+
+  });
+});
