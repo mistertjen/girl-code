402312ffa92dc1dd94ffa06954378d89bbe303a8
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-10-05 18:29:06 +0200

    made buttons in footer bounce on hover too in main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.log("Hi Girl Coder, please make sure you join our community at meetup.com/GirlCode. Boys are welcome too!");
 
 $(document).ready(function() {
-  $('.social-button').hover(function() {
+  $('.social-button, .social-button-footer').hover(function() {
     $(this).effect( 'bounce', { times: 1 }, 'slow');
 
   });
